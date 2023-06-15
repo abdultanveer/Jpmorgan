6614783579a9fd89e24c8220e3073d6f43e94495
Abdul <abdul.tanveer@gmail.com>
2023-06-15 10:27:01 +0530

    listview with arrayadapter

--- app/src/main/java/com/example/jpmorgan/recyclerview/RecyclerActivity.java
+++ app/src/main/java/com/example/jpmorgan/recyclerview/RecyclerActivity.java
@@ -3,14 +3,31 @@ package com.example.jpmorgan.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.jpmorgan.R;
 
+import java.sql.Array;
+
 public class RecyclerActivity extends AppCompatActivity {
     String[] languages = {"english","urdu","kannada","hindi","arabic"};
+    ListView countListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+        countListView = findViewById(R.id.countrieslistview);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, //layout is of each
+                android.R.id.text1, //textview of the above layout
+                languages);
+        countListView.setAdapter(adapter);
     }
 }
