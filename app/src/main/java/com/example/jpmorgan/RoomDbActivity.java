33a133fac781030bd7efa79d9a05efb892d2d510
Abdul <abdul.tanveer@gmail.com>
2023-06-15 15:17:54 +0530

    room db

---
+++ app/src/main/java/com/example/jpmorgan/RoomDbActivity.java
@@ -0,0 +1,63 @@
+package com.example.jpmorgan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.jpmorgan.roomdb.AppDatabase;
+import com.example.jpmorgan.roomdb.AppExecutors;
+import com.example.jpmorgan.roomdb.Person;
+
+public class RoomDbActivity extends AppCompatActivity {
+
+    EditText name, email, pincode, city, phoneNumber;
+    Button button;
+    int mPersonId;
+    Intent intent;
+    private AppDatabase mDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_db);
+        initViews();
+        mDb = AppDatabase.getInstance(this);
+    }
+
+
+    private void initViews() {
+        name = findViewById(R.id.edit_name);
+        email = findViewById(R.id.edit_email);
+        pincode = findViewById(R.id.edit_pincode);
+        city = findViewById(R.id.edit_city);
+        phoneNumber = findViewById(R.id.edit_number);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSaveButtonClicked();
+            }
+        });
+    }
+
+    public void onSaveButtonClicked() {
+        final Person myPerson = new Person(
+                name.getText().toString(),
+                email.getText().toString(),
+                phoneNumber.getText().toString(),
+                pincode.getText().toString(),
+                city.getText().toString());
+
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                mDb.personDao().insertPerson(myPerson); //inserting data asynchronously on a seperate thread
+            }
+        });
+
+    }
+}
