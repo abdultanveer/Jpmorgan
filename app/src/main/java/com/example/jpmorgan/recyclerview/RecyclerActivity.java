1dfaaa978fd60d3da056ad214b135b097b4d950c
Abdul <abdul.tanveer@gmail.com>
2023-06-15 11:09:38 +0530

    recyclerview -3

--- app/src/main/java/com/example/jpmorgan/recyclerview/RecyclerActivity.java
+++ app/src/main/java/com/example/jpmorgan/recyclerview/RecyclerActivity.java
@@ -1,39 +1,44 @@
 package com.example.jpmorgan.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.jpmorgan.R;
 
-import java.sql.Array;
-
 public class RecyclerActivity extends AppCompatActivity
         implements AdapterView.OnItemClickListener {
-    String[] languages = {"english","urdu","kannada","hindi","arabic"};
-    RecyclerView countListView;
+    String[] languages = {"english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",
+            "english","urdu","kannada","hindi","arabic",};
+    RecyclerView countRecyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
-        countListView = findViewById(R.id.countrieslistview);
+        countRecyView = findViewById(R.id.countrieslistview);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, //layout is of each
-                android.R.id.text1, //textview of the above layout
-                languages);
+
         LangAdapter langAdapter = new LangAdapter(languages);
+        countRecyView.setLayoutManager(new LinearLayoutManager(this));
+        countRecyView.setAdapter(langAdapter);
 
        // countListView.setOnItemClickListener(this);
 
